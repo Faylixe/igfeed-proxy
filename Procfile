2f926d24344ab3aa726685eb49dceb2c31e2b921
Félix Voituret <fvoituret@deezer.com>
2021-04-08 21:35:53 +0200

    🎨  finalizes code format

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: hypercorn --workers 1 --worker-class uvloop --access-logfile - --bind 0.0.0.0:${PORT:-5000} igfp:igfp
+web: hypercorn --workers 1 --worker-class uvloop --access-logfile - --bind 0.0.0.0:${PORT:-5000} igfp:api
--- igfp/__init__.py
+++ igfp/__init__.py
@@ -1,15 +1,16 @@
 import logging
-
 from enum import Enum
 from functools import lru_cache
 from time import time
-from typing import Any, List, Optional, cast
+from typing import Any, List, Optional, Union, cast
 
 from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from httpx import AsyncClient, HTTPStatusError, Response
-from pydantic import BaseModel, BaseSettings
+from pydantic import AnyHttpUrl, BaseModel, BaseSettings, validator
+from starlette.responses import RedirectResponse
 
-igfp = FastAPI(docs_url=None, openapi_url=None, redoc_url=None)
+api = FastAPI(docs_url=None, openapi_url=None, redoc_url=None)
 igapi = AsyncClient(
     base_url="https://api.instagram.com",
     headers={"Accept": "application/json"},
@@ -44,6 +45,7 @@ class SettingsModel(BaseSettings):
     APPLICATION_ID: str
     APPLICATION_SECRET: str
     DOMAIN: str
+    CORS_ORIGINS: Union[str, List[AnyHttpUrl]] = []
     MEDIA_FIELDS: str = (
         "caption,"
         "id,"
@@ -66,12 +68,27 @@ class SettingsModel(BaseSettings):
     MEDIA_REFRESH_DELAY: int = 60 * 5
     """ Refresh media every 5 minutes to avoid API rate limiting. """
     PROTOCOL: ProtocolEnum = ProtocolEnum.HTTPS
-    SCOPES: List[str] = ["user_media", "user_profile"]
+    # NOTE: Consider using Enumeration to avoid wrong scope usage.
+    SCOPES: Union[str, List[str]] = ["user_media", "user_profile"]
     TOKEN_REFRESH_DELAY: int = 60 * 60 * 24 * 30
     """ Refresh every 30 days, to be sure we do not miss the window without spamming. """
 
     class Config:
         env_prefix = "IGFP_"
+
+    @validator("CORS_ORIGINS", pre=True)
+    def _assemble_cors_origins(
+        _, origins: Union[str, List[AnyHttpUrl]]
+    ) -> List[AnyHttpUrl]:
+        if isinstance(origins, str):
+            return [AnyHttpUrl(origin.strip()) for origin in origins.split(",")]
+        return origins
+
+    @validator("SCOPES", pre=True)
+    def _assemble_scopes(_, scopes: Union[str, List[str]]) -> List[str]:
+        if isinstance(scopes, str):
+            return [scope.strip() for scope in scopes.split(",")]
+        return scopes
 
 
 class ContextModel(BaseModel):
@@ -131,13 +148,20 @@ async def AccessToken(
     return cast(str, context.token)
 
 
-@igfp.on_event("startup")
+@api.on_event("startup")
 def startup(
     # NOTE: FastAPI doesn't support event callback dependency injection yet :'(
     # redirect_uri: str = Depends(RedirectURI),
     # settings: SettingsModel = Depends(Settings),
 ) -> None:
     settings = Settings()
+    api.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["GET"],
+        allow_headers=["*"],
+    )
     scopes = ",".join(settings.SCOPES)
     logger.info(
         f"To activate Instagram feed proxy please authenticate to "
@@ -149,20 +173,20 @@ def startup(
     )
 
 
-@igfp.get("/")
-@igfp.post("/unauthorize")
-@igfp.post("/remove")
-async def placeholder() -> None:
-    pass
+@api.get("/")
+@api.post("/unauthorize")
+@api.post("/remove")
+async def home() -> RedirectResponse:
+    return RedirectResponse(api.url_path_for("media"))
 
 
-@igfp.get("/authorize")
+@api.get("/authorize")
 async def authorize(
     code: str,
     context: ContextModel = Depends(Context),
     redirect_uri: str = Depends(RedirectURI),
     settings: SettingsModel = Depends(Settings),
-) -> None:
+) -> RedirectResponse:
     if context.token is not None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     # NOTE: Retrieve initial short lived token.
@@ -177,6 +201,7 @@ async def authorize(
         },
     )
     raise_for_status(response)
+    # TODO: add a log message ?
     # NOTE: exchange for 60 days long token.
     response = await iggraph.get(
         "/access_token",
@@ -187,18 +212,18 @@ async def authorize(
         },
     )
     raise_for_status(response)
+    # TODO: add a log message ?
     context.token = response.json().get("access_token")
     context.token_refreshed = time()
-    # TODO: redirect to home.
+    return RedirectResponse(api.url_path_for("media"))
 
 
-@igfp.get("/media")
+@api.get("/media")
 async def media(
     access_token: str = Depends(AccessToken),
     context: ContextModel = Depends(Context),
     settings: SettingsModel = Depends(Settings),
 ) -> Any:
-    """ """
     now = time()
     if now - context.media_refreshed > settings.MEDIA_REFRESH_DELAY:
         response = await iggraph.get(
